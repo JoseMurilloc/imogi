6276f6db6aa8366d037c8e0e6f4c7bbfab64d30d
johelder <johelder.arj@gmail.com>
2022-08-12 00:30:42 -0300

    feat: adding background color to app

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
   * {
@@ -7,9 +7,11 @@ export const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
   }
 
-  body {
-    font-family: Arial, Helvetica, sans-serif;
-    font-size: 16px;
-    color: #E1E1E6;
+  html {
+    font-size: 62.5%;
   }
-`
+
+  body {
+    background-color: #1B1D24;
+  }
+`;
