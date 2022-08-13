e543ba2310848c8ab5f154a7912ff2101ca1eaac
johelder <johelder.araujo@pede.ai>
2022-08-13 14:42:09 -0300

    feat: create toast component

---
+++ src/components/Toast/index.tsx
@@ -0,0 +1,26 @@
+import { useEffect } from 'react';
+import { useToast } from '../../hooks/toast';
+
+import * as S from './styles';
+
+interface IToastProps {
+  message: string;
+}
+
+export const Toast = ({ message }: IToastProps) => {
+  const { setShouldRenderToast } = useToast();
+
+  useEffect(() => {
+    const timeToDisplayToast = setInterval(() => {
+      setShouldRenderToast(false);
+    }, 1000);
+
+    return () => clearTimeout(timeToDisplayToast);
+  }, []);
+
+  return (
+    <S.Container>
+      <span>{message}</span>
+    </S.Container>
+  );
+};
---
+++ src/components/Toast/styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Container = styled.output`
+  position: absolute;
+  bottom: 5%;
+  left: 50%;
+  transform: translate(-50%, 0);
+
+  background-color: rgba(160, 38, 235, 0.5);
+
+  padding: 0.3rem 0.6rem;
+
+  border-radius: 0.5rem;
+
+  span {
+    color: #fff;
+    font-family: sans-serif;
+  }
+`;
