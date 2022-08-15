9fb3440dce0cd2bb4cac4d01599469539af54d1f
johelder <johelder.araujo@pede.ai>
2022-08-15 15:59:27 -0300

    feat: styling scroll bar

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -13,5 +13,16 @@ export const GlobalStyle = createGlobalStyle`
 
   body {
     background-color: #1B1D24;
+    overflow: hidden;
+  }
+
+  ::-webkit-scrollbar {
+    background-color: #1B1D24;
+    width: 1rem;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background-color: #121214;
+    border-radius: 2rem;
   }
 `;
