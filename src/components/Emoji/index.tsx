ea9cefe68dcea9eb9f4888aa7d56acdbf4494664
johelder <johelder.araujo@pede.ai>
2022-08-15 15:55:56 -0300

    feat: create Emoji component

--- src/components/Emoji/index.tsx
+++ src/components/Emoji/index.tsx
@@ -1,3 +1,30 @@
-export const Emoji = () => {
-  return <h1>emoji</h1>;
+import { MouseEvent, memo } from 'react';
+
+import { useToast } from '../../hooks/toast';
+
+import * as S from './styles';
+
+interface IEmojiProps {
+  codePoint: string;
+}
+
+const Emoji = ({ codePoint }: IEmojiProps) => {
+  const { showToast } = useToast();
+
+  const handleCopyEmojiToClipBoard = (event: MouseEvent<HTMLElement>) => {
+    const target = event.target as HTMLInputElement;
+    navigator.clipboard.writeText(target.value);
+
+    showToast('Copied!');
+  };
+
+  return (
+    <S.Container
+      onClick={handleCopyEmojiToClipBoard}
+      type="button"
+      value={String.fromCodePoint(parseInt(codePoint, 16))}
+    />
+  );
 };
+
+export default memo(Emoji);
---
+++ src/components/Emoji/styles.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const Container = styled.input`
+  width: 4rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  background: none;
+  border: none;
+
+  border-radius: 0.3rem;
+
+  transition: background-color 400ms;
+
+  font-size: 2.2rem;
+
+  &:hover {
+    background-color: #4a5159;
+  }
+`;
