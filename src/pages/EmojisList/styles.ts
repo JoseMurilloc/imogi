34ae9cd9895d910089ec1c218e20817adedd25a5
johelder <johelder.araujo@pede.ai>
2022-08-20 11:58:19 -0300

    refactor: render emojis a unordered list

--- src/pages/EmojisList/styles.ts
+++ src/pages/EmojisList/styles.ts
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
 
+interface IEmojisContainerProps {
+  formattedToRenderOneLine: boolean;
+}
+
 export const Container = styled.div`
   width: 100%;
   min-height: 90%;
@@ -8,14 +12,21 @@ export const Container = styled.div`
   flex-direction: column;
 `;
 
-export const EmojisContainer = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-
-  padding: 1rem 1rem 1rem 2rem;
-
-  align-items: center;
-  justify-content: space-between;
-
+export const EmojisContainer = styled.div<IEmojisContainerProps>`
   overflow-y: scroll;
+
+  ul {
+    width: fit-content;
+    display: flex;
+    flex-wrap: wrap;
+    flex-grow: 1;
+
+    list-style: none;
+
+    padding: 1rem 1rem 1rem 2rem;
+
+    align-items: center;
+    justify-content: ${({ formattedToRenderOneLine }) =>
+      formattedToRenderOneLine ? 'flex-start' : 'space-between'};
+  }
 `;
