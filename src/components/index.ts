72fccaedc5e2034c185a2dc9bc6b7be6d365415d
johelder <johelder.araujo@pede.ai>
2022-08-20 11:48:07 -0300

    feat: adding components to index

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,7 @@
 import Emoji from './Emoji';
 import { SearchInput } from './SearchInput';
+import { CategoriesTabNavigation } from './CategoriesTabNavigation';
+import { CategoryButton } from './CategoryButton';
 import { Toast } from './Toast';
 
-export { Emoji, SearchInput, Toast };
+export { Emoji, SearchInput, Toast, CategoriesTabNavigation, CategoryButton };
