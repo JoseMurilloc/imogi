1ed0bc0d22c1c60e6cfa11b952da4fc4450ef32f
johelder <johelder.arj@gmail.com>
2022-08-21 11:01:23 -0300

    refactor: changing access_key to a constant

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,13 @@
 import axios from 'axios';
 
+const ACCESS_KEY = 'b20eecade4d3d144a259a0aa80a32e9ea48d66e4';
+
 export const api = axios.create({
   baseURL: 'https://emoji-api.com',
   headers: {
     'Access-Control-Allow-Origin': '*',
   },
   params: {
-    access_key: 'b20eecade4d3d144a259a0aa80a32e9ea48d66e4',
+    access_key: ACCESS_KEY,
   },
 });
