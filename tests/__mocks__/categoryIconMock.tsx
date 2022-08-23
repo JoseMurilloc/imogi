5debb0832b183d96d9145fcf437fb09a9dc13f43
johelder <johelder.arj@gmail.com>
2022-08-22 23:43:43 -0300

    test: create category icons mock

---
+++ tests/__mocks__/categoryIconMock.tsx
@@ -0,0 +1,6 @@
+import { FaRegSmile } from 'react-icons/fa';
+
+export const iconsMock = {
+  active: <FaRegSmile color="#a026eb" size={18} />,
+  disabled: <FaRegSmile color="#515156" size={18} />,
+};
