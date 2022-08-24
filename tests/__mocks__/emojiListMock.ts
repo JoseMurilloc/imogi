8539a3f76c74629af35a55cba544843a13a013c9
johelder <johelder.araujo@pede.ai>
2022-08-24 17:30:16 -0300

    test: create emoji list mock

---
+++ tests/__mocks__/emojiListMock.ts
@@ -0,0 +1,22 @@
+export const emojiListMock = [
+  {
+    slug: 'globe-showing-europe-africa',
+    codePoint: '1F30D',
+    group: 'travel-place',
+  },
+  {
+    slug: 'globe-showing-americas',
+    codePoint: '1F30E',
+    group: 'travel-places',
+  },
+  {
+    slug: 'globe-showing-asia-australia',
+    codePoint: '1F30F',
+    group: 'travel-places',
+  },
+  {
+    slug: 'globe-with-meridians',
+    codePoint: '1F310',
+    subGroup: 'place-map',
+  },
+];
