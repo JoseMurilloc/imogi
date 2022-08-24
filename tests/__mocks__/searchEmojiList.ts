927a2a383a2d2d93741049a4b8f8b3ac763d0cd6
johelder <johelder.araujo@pede.ai>
2022-08-24 17:30:35 -0300

    test: create search emoji list

---
+++ tests/__mocks__/searchEmojiList.ts
@@ -0,0 +1,17 @@
+export const searchEmojiListMock = [
+  {
+    slug: 'desktop-computer',
+    codePoint: '1F5A5 FE0F',
+    group: 'objects',
+  },
+  {
+    slug: 'computer-mouse',
+    codePoint: '1F5B1 FE0F',
+    group: 'objects',
+  },
+  {
+    slug: 'computer-disk',
+    codePoint: '1F4BD',
+    group: 'objects',
+  },
+];
