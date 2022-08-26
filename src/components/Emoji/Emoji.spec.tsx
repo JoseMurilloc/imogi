2b4c9ca18317df79aeab253cb4c3917c6d453495
johelder <johelder.araujo@pede.ai>
2022-08-25 22:34:17 -0300

    test: create clipboard emoji test

--- src/components/Emoji/Emoji.spec.tsx
+++ src/components/Emoji/Emoji.spec.tsx
@@ -1,6 +1,18 @@
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Emoji from '.';
-import { SearchInput } from '../SearchInput';
+
+jest.mock('../../hooks/toast', () => {
+  return {
+    useToast() {
+      return {
+        showToast() {
+          return jest.fn();
+        },
+      };
+    },
+  };
+});
 
 describe('Emoji Component', () => {
   it('should render a emoji from a codePoint', () => {
@@ -10,7 +22,15 @@ describe('Emoji Component', () => {
     expect(emojiComponent).toHaveAttribute('value', '🖥');
   });
 
-  it('should able to copy a emoji to a clipboard', () => {
-    const { getByTestId } = render(<SearchInput searchTerm="fake-term" />);
+  it('should able to copy a emoji to a clipboard', async () => {
+    const user = userEvent.setup();
+    const { getByTestId } = render(<Emoji codePoint="1F5A5 FE0F" />);
+    const emojiComponent = getByTestId('emojiInput');
+
+    await user.click(emojiComponent);
+
+    const clipboardContent = await navigator.clipboard.readText();
+
+    expect(clipboardContent).toBe('🖥');
   });
 });
